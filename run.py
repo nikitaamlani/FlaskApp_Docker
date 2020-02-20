0208a9cb3a125d729a933f4bba69ced8226d8d02
nikitaamlani <56655284+nikitaamlani@users.noreply.github.com>
2020-02-19 20:15:24 -0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+
+@app.route('/time')
+
+def time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return(current_time)
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
